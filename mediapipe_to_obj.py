4662f77be7ef2442f55bab1a74a5ea632ad6fa51
DrCyanide <flagezooke@gmail.com>
2023-09-05 06:49:57 -0600

    Rotates the final object so it's not tilted

--- mediapipe_to_obj.py
+++ mediapipe_to_obj.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import math
 import cv2
 import numpy as np
 import mediapipe as mp
@@ -162,18 +163,69 @@ def normalize_keypoints(keypoints3d):
     #  Landmarks labeled
     #  https://github.com/google/mediapipe/blob/a908d668c730da128dfa8d9f6bd25d519d006692/mediapipe/modules/face_geometry/data/canonical_face_model_uv_visualization.png
     center = keypoints3d[0]
-    keypoints3d = keypoints3d - center
-    axis1 = keypoints3d[165] - keypoints3d[391] # 165 = left side between nose and lip, # right side between nose and lip (one vertex off center)
-    axis2 = keypoints3d[2] - keypoints3d[0] # 2 = below nose, 0 = above lip (only 1 vertex apart)
+    keypoints3d = keypoints3d - center # This centering is undone by some later step
+    # axis1 = keypoints3d[165] - keypoints3d[391] # 165 = left side between nose and lip, # right side between nose and lip (one vertex off center)
+    # axis2 = keypoints3d[2] - keypoints3d[0] # 2 = below nose, 0 = above lip (only 1 vertex apart)
+    axis1 = keypoints3d[454] - keypoints3d[234] # side of face to side of face
+    axis2 = keypoints3d[10] - keypoints3d[152] # forehead to chin
     axis3 = np.cross(axis2,axis1)
     axis3 = axis3/np.linalg.norm(axis3)
     axis2 = axis2/np.linalg.norm(axis2)
-    axis1 = np.cross(axis3, axis2)
+    axis1 = np.cross(axis3, axis2) # Should be redundant
     axis1 = axis1/np.linalg.norm(axis1)
-    U = np.array([axis3,axis2,axis1])
+    U = np.array([axis3,axis2,axis1]) # Would changing this order help correctly orient the object?
     keypoints3d = keypoints3d.dot(U)
     keypoints3d = keypoints3d - keypoints3d.mean(axis=0) # Doesn't seem to make a difference
+
     return keypoints3d
+
+def align_keypoints_to_grid(keypoints3d):
+    # Center at keypoints3d[0]
+    # Put chin and forehead (keypoints3d[152] and keypoints3d[10]) on Z axis
+    # Put left and right cheeks (keypoints3d[234] and keypoints3d[454]) on X axis
+    #  Landmarks labeled
+    #  https://github.com/google/mediapipe/blob/a908d668c730da128dfa8d9f6bd25d519d006692/mediapipe/modules/face_geometry/data/canonical_face_model_uv_visualization.png
+    # If needed, center the face 
+    if np.count_nonzero(keypoints3d[0]) > 0: # Check if the values are all zeros
+        keypoints3d = keypoints3d - keypoints3d[0]
+
+    # Rotate the face left-to-right to be straight up-and-down
+    adj_side, opp_side = keypoints3d[10][1], keypoints3d[10][2] # Forehead
+    angle = math.atan2(opp_side, adj_side)
+    keypoints3d = rotate_around_origin(keypoints3d, 2, 1, angle)
+
+    # Rotate the face forward-and-back
+    adj_side, opp_side = keypoints3d[9][1], keypoints3d[9][0] # Center of brow
+    angle = math.atan2(opp_side, adj_side)
+    keypoints3d = rotate_around_origin(keypoints3d, 0, 1, angle)
+
+    # Rotate the face to be looking straight ahead
+    face_left = keypoints3d[454]
+    face_right = keypoints3d[234]
+    # Need a midpoint between these two to rotate between
+    middle = (face_left + face_right) / 2
+    keypoints3d = keypoints3d - middle
+    # Re-determine the left/right side
+    face_left = keypoints3d[454]
+    face_right = keypoints3d[234]
+    adj_side, opp_side = face_left[0], face_left[2]
+    angle = math.atan2(opp_side, adj_side)
+    keypoints3d = rotate_around_origin(keypoints3d, 2, 0, angle)
+    
+    # Return center it's original location
+    keypoints3d = keypoints3d - keypoints3d[0]
+    return keypoints3d
+
+def rotate_around_origin(keypoints3d, opp_side, adj_side, angle):
+    for row in keypoints3d:
+        a0 = math.cos(angle) * row[opp_side] - math.sin(angle) * row[adj_side]
+        a1 = math.cos(angle) * row[adj_side] + math.sin(angle) * row[opp_side]
+        row[opp_side] = a0
+        row[adj_side] = a1
+    return keypoints3d
+
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
 
 def main():
     parser = argparse.ArgumentParser(prog="Mediapipe to OBJ", description="Covert 2D pictures to 3D meshes")
@@ -228,7 +280,8 @@ def main():
 
     # keypoints3d already has a face that's more round than the original
     vertices = normalize_keypoints(keypoints3d)
-    # vertices = keypoints3d
+    # Rotate the vertices so the face isn't at an odd angle
+    vertices = align_keypoints_to_grid(vertices)
     
     filename =  os.path.splitext(os.path.basename(img_path))[0] # the name without the extension
     obj_name =  "./results/%s.obj" % filename
